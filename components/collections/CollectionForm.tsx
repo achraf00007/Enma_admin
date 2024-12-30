425bdc7d6a68d9444d3ff78aa741517e1524bb7a
achraf00007 <achrafelmardi473@gmail.com>
2024-12-30 15:46:22 +0100

    feat(collections): add form to create a new collection

---
+++ components/collections/CollectionForm.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import { z } from "zod";
+import { Separator } from "../ui/separator";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "../ui/textarea";
+import ImageUpload from "../custom ui/ImageUpload";
+import { useRouter } from "next/navigation";
+
+const formSchema = z.object({
+  title: z.string().min(2).max(20),
+  description: z.string().min(2).max(500).trim(),
+  image: z.string(),
+});
+
+export default function CollectionForm() {
+    const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+      image: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <div className="p-10">
+      <p className="text-heading2-bold">Create collection</p>
+      <Separator className="bg-grey-1 mt-4 mb-7" />
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Title</FormLabel>
+                <FormControl>
+                  <Input placeholder="Title" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Textarea placeholder="Description" {...field} rows={5} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="image"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Image</FormLabel>
+                <FormControl>
+                  <ImageUpload 
+                    value={field.value ? [field.value] : []}
+                    onChange={(url)=> field.onChange(url)}
+                    onRemove={() => field.onChange("")}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        <div className="flex gap-10">
+          <Button type="button" onClick={() => router.push('/collections') } variant="outline" className="">Discard</Button>
+          <Button type="submit" className="bg-blue-1 text-white">Create</Button>
+        </div>
+        </form>
+      </Form>
+    </div>
+  );
+}
