c04f0a061e7af7e5a8537d81f8359e15d2e18ea2
achraf00007 <achrafelmardi473@gmail.com>
2025-01-02 20:30:20 +0100

    feat(collections): Delete collection by id

---
+++ components/custom ui/Delete.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { Trash } from "lucide-react";
+import { Button } from "../ui/button";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+interface DeleteProps {
+    id: string;
+}
+
+export default function Delete({ id }:DeleteProps) {
+    const [loading, setLoading] = useState(false);
+    const onDelete = async () => {
+        try {
+            setLoading(true);
+            const response = await fetch(`/api/collections/${id}`, {
+                method: "DELETE",
+            });
+
+            if(response.ok) {
+                window.location.reload();
+                toast.success("Collection deleted successfully");
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error("Something went wrong! Please try again");
+        }
+    };
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger>
+        <Button className="bg-red-1 text-white">
+          <Trash className="h4 w-4" />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent className="bg-white">
+        <AlertDialogHeader>
+          <AlertDialogTitle className="text-red-1">Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            collection.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction className="bg-red-1 text-white-1" onClick={onDelete}>Delete</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
