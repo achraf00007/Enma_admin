0992b6b1544c41b6cbfa96bf4369168b23a5111d
achraf00007 <achrafelmardi473@gmail.com>
2025-01-03 09:45:04 +0100

    chore: add loading to the collections page

--- app/(dashboard)/collections/page.tsx
+++ app/(dashboard)/collections/page.tsx
@@ -2,6 +2,7 @@
 
 import { columns } from "@/components/collections/CollectionColumns";
 import { DataTable } from "@/components/custom ui/DataTable";
+import Loader from "@/components/custom ui/Loader";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { Plus } from "lucide-react";
@@ -9,41 +10,47 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function Collections() {
-
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [collections, setCollections] = useState([]);
   const router = useRouter();
 
   const getCollections = async () => {
     try {
-      setLoading(true);
-      const response = await fetch("/api/collections",{ method: "GET" });
+      // setLoading(true);
+      const response = await fetch("/api/collections", { method: "GET" });
       const data = await response.json();
       setCollections(data);
       setLoading(false);
     } catch (err) {
       console.log("[Collections_GET]", err);
     }
-  }
+  };
 
   useEffect(() => {
     getCollections();
   }, []);
 
-  return(
-    <div className="px-10 py-5">
-      <div className="flex justify-between items-center">
-        <p className="text-heading2-bold text-primary-700">
-          Collections
-        </p>
-        <Button className="bg-primary-700 text-white" onClick={() => router.push("/collections/new")}>
-          <Plus className="h4 w-4 mr-2" /> New collection
-        </Button>
-      </div>
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="px-10 py-5">
+          <div className="flex justify-between items-center">
+            <p className="text-heading2-bold text-primary-700">Collections</p>
+            <Button
+              className="bg-primary-700 text-white"
+              onClick={() => router.push("/collections/new")}
+            >
+              <Plus className="h4 w-4 mr-2" /> New collection
+            </Button>
+          </div>
 
-      <Separator className="bg-primary-300 mt-4 mb-7" />
+          <Separator className="bg-primary-300 mt-4 mb-7" />
 
-      <DataTable columns={columns} data={collections} searchKey="title" />
-    </div>
+          <DataTable columns={columns} data={collections} searchKey="title" />
+        </div>
+      )}
+    </>
   );
 }
