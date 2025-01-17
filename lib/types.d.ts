30e6bab0168e7dbbeaf80b7cedb16486cc728a69
achraf00007 <achrafelmardi473@gmail.com>
2025-01-17 16:55:38 +0100

    feat(Orders): add order details page and API route for fetching order information

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,32 +1,39 @@
 type CollectionType = {
-    _id: string;
-    title: string;
-    description: string;
-    image: string;
-    products: ProductType[];
-}
+  _id: string;
+  title: string;
+  description: string;
+  image: string;
+  products: ProductType[];
+};
 
 type ProductType = {
-    _id: string;
-    title: string;
-    description: string;
-    media: [string];
-    price: number;
-    category: string;
-    collections: [CollectionType];
-    tags: [string];
-    sizes: [string];
-    colors: [string];
-    price: number;
-    cost: number;
-    createdAt: Date;
-    updatedAt: Date;
-}
+  _id: string;
+  title: string;
+  description: string;
+  media: [string];
+  price: number;
+  category: string;
+  collections: [CollectionType];
+  tags: [string];
+  sizes: [string];
+  colors: [string];
+  price: number;
+  cost: number;
+  createdAt: Date;
+  updatedAt: Date;
+};
 
 type OrderColumnType = {
-    _id: string;
-    customer: string;
-    products: number;
-    totalAmount: number;
-    createdAt: string;
-}
+  _id: string;
+  customer: string;
+  products: number;
+  totalAmount: number;
+  createdAt: string;
+};
+
+type OrderItemType = {
+  product: ProductType;
+  color: string;
+  size: string;
+  quantity: number;
+};
